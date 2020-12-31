294b1b35a3a59e5f2cfac2e268d16688a0975b4a
Choilocal <hyunchoi@hist.co.kr>
2020-12-31 23:14:23 +0900

    Query 수정, css path 수정

--- src/main/java/sample/controller/BoardController.java
+++ src/main/java/sample/controller/BoardController.java
@@ -2,6 +2,7 @@ package sample.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import sample.dto.BoardDto;
 import sample.service.BoardService;
@@ -14,6 +15,7 @@ public class BoardController {
     @Autowired
     private BoardService boardService;
 
+    @RequestMapping("/board/openBoardList.do")
     public ModelAndView openBoardList() throws Exception {
 
         ModelAndView mv = new ModelAndView("/board/boardList");
